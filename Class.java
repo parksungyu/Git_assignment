61ebf2c17295bade79e18f47ca9cbdeb900952ff
Park sun gyu <qkrtns95@naver.com>
2019-01-12 17:29:12 +0900

    Create Class.java

---
+++ Class.java
@@ -0,0 +1,35 @@
+//필드를 private 으로 막아준다
+//바꾸고 싶은경우 getter/setter를 사용해서 접근하여준다.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+package Git_assignment;
+
+public class Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
